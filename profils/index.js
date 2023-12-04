070f2fd02f826fd0245702396e29bd9a1c15a20d
kurtounet <80813899+kurtounet@users.noreply.github.com>
2023-12-04 18:51:13 +0100

    update 2

--- profils/index.js
+++
@@ -1,9 +0,0 @@
-function openProfil() {
-    let profil = document.getElementById('profil');
-    profil.style.display = 'block';
-}
-
-function closeProfil() {
-    let profil = document.getElementById('profil');
-    profil.style.display = 'none';
-}
